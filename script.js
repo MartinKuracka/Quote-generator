1f8eb29761859f91cf25acded22ed46184d20b54
martin kuracka <m.kuracka@gmail.com>
2021-02-19 13:26:26 +0100

    code cleanup

--- script.js
+++ script.js
@@ -5,25 +5,23 @@ const authorText = document.getElementById('author');
 const facebookButton = document.getElementById('facebook');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+let errorLoadingQuoteCount = 0;
 
-// show loading
-const loading = () => {
+const showLoaderSpinner = () => {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// hide loading
-const complete = () => {
+const hideLoaderSpinner = () => {
     if (!loader.hidden) {
         quoteContainer.hidden = false;
         loader.hidden = true
     }
 }
 
-
 // get quote from API
-async function getQuote() {
-    loading();
+async function getQuoteFromApi() {
+    showLoaderSpinner();
     const proxyUrl = 'https://cors.bridged.cc/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';   
     try {
@@ -31,30 +29,27 @@ async function getQuote() {
         const data = await response.json();
         console.log(data);
         //  if author is blank, add 'Unknown'
-        if (data.quoteAuthor === '') {
-            authorText.innerText = 'Unknown'
-        }else authorText.innerText = data.quoteAuthor;
+        if (data.quoteAuthor === '' 
+            ? authorText.innerText = 'Author Unknown'
+            : authorText.innerText = data.quoteAuthor)
         // reduce font sizefor long quote
-        if (data.quoteText.length > 80) {
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote')
-        }
+        if (data.quoteText.length > 90
+            ? quoteText.classList.add('long-quote')
+            : quoteText.classList.remove('long-quote'));        
         quoteText.innerText = data.quoteText   
         // stop loader and show the quote
-        complete();        
-      } catch (error) {         
-        getQuote();
-    }
+        hideLoaderSpinner();       
+        errorLoadingQuoteCount = 0;  
+        console.log(errorLoadingQuoteCount);
+    }   catch (error) {  
+            errorLoadingQuoteCount += 1;          
+            if (errorLoadingQuoteCount === 20
+            ? alert('can not load the quote from the server')
+            : getQuoteFromApi());        
+        }
 }
 
-// facebookQuotePLacer = () => {
-//     const quote = quoteText.innerText;
-//     const author = authorText.innerText;
-//     const fbUrl = 
-// }
-
-const facebookQuotePLacer = () => {
+const facebookQuotePlacer = () => {
     console.log('shared');
     FB.ui({      
       display: 'popup',
@@ -64,10 +59,9 @@ const facebookQuotePLacer = () => {
   }
 
 
-// event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
-facebookButton.addEventListener('click', facebookQuotePLacer);
-
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuoteFromApi);
+facebookButton.addEventListener('click', facebookQuotePlacer);
 
 // On load
-getQuote();
+getQuoteFromApi();
